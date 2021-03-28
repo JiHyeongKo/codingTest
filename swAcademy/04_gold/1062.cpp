b163d421d42718b32459ba8cdd3c4948fd5e5a5b
JiHyeongKo <72496027+JiHyeongKo@users.noreply.github.com>
2021-03-28 21:50:17 +0900

    Update 1062.cpp

--- swAcademy/04_gold/1062.cpp
+++ swAcademy/04_gold/1062.cpp
@@ -1,85 +1,95 @@
-// 아직 푸는중....
-
 #include <iostream>
-using namespace std;
-int n, k, ans = 0;
-bool alpha[26] = {0,};
-string str[50];
+#include <string>
+#include <vector>
 
-void solve(int temp, int idx)
+using namespace std;
+
+vector<string> s;
+bool chk[26];
+int N, K;
+int maxv = 0;
+vector<int> v;
+
+void dfs(int num)
 {
-    if(temp == k - 5)
+    // 5개 제외하고 모두 고를경우
+    if (num == K - 5)
     {
         int cnt = 0;
-
-        for(int i = 0; i < n; i++)
+        for (string es : s)
         {
-            int cc = 0;
-            
-            for(int j = 0; j < str[i].size(); j++)
+            bool sw = false;
+            for (int i = 0; i < es.size(); i++)
             {
-                if(alpha[str[i][j] - 'a'] == 1) cc++;
-
-                else break;
+                char ec = es[i];
+                if (chk[ec - 'a'] == false)
+                {
+                    sw = 1;
+                    break;
+                }
             }
-
-            if(cc == str[i].size()) cnt++;
+            if (sw == false)
+                cnt++;
         }
 
-        if(ans < cnt) 
-            ans = cnt;
+        maxv = max(maxv, cnt);
 
         return;
     }
 
-    for (int i = idx; i < 26; i++)
+    // 알파벳 중복체크 후 추가
+    // 조합이므로 리스트 맨 뒤에서 빼기
+    int lastv = -1;
+    if (!v.empty())
+        lastv = v.back();
+
+    for (int i = lastv + 1; i < 26; i++)
     {
-        if(alpha[i] == 0)
-        {
-            alpha[i] = 1;
-            solve(temp+1, i+1);
-            alpha[i] = 0;
-        }
+        if (chk[i])
+            continue;
+        chk[i] = 1;
+        v.push_back(i);
+
+        dfs(num + 1);
+
+        chk[i] = 0;
+        v.pop_back();
     }
 }
-
 int main()
 {
-    cin >> n >> k;
-    
-    if(n < 5)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    fill(chk, chk + 26, 0);
+
+    cin >> N >> K;
+
+    // 문자열 잘라서 보관
+    for (int i = 0; i < N; i++)
     {
-        cout << 0 << "\n";
+        string es;
+        cin >> es;
+        // 잘라서 보관
+        s.push_back(es.substr(4, es.size() - 8));
+    }
+
+    chk['a' - 'a'] = 1;
+    chk['n' - 'a'] = 1;
+    chk['t' - 'a'] = 1;
+    chk['i' - 'a'] = 1;
+    chk['c' - 'a'] = 1;
+
+    // 5개 이하일경우 문자열 읽을 수 없음
+    if (K < 5)
+    {
+        cout << 0 << '\n';
         return 0;
     }
 
-    if(n >= 26)
-    {
-        cout << n << "\n";
-        return 0;
-    }
-    
-    alpha['a' - 'a'] = 1;
-    alpha['n' - 'a'] = 1;
-    alpha['t' - 'a'] = 1;
-    alpha['i' - 'a'] = 1;
-    alpha['c' - 'a'] = 1;
-    
-    for(int i = 0; i < n; i++)
-        cin >> str[i];
+    dfs(0);
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < str[i].size(); j++)
-        {
-            if(str[i][j] == 'a' || str[i][j] == 'n' || str[i][j] == 't' || str[i][j] == 'i' || str[i][j] == 'c')
-                continue;
-
-            alpha[str[i][j] - 'a'] = 1;
-        }
-
-    solve(0, 0);
-
-    cout << ans << "\n";
+    cout << maxv << '\n';
 
     return 0;
 }
